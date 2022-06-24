fbcea516ff0c0b01099ca89f0395be04ff075db0
BildadOwuor <bildadowuor@gmail.com>
2022-06-24 15:18:02 +0300

    updates

--- index.js
+++ index.js
@@ -2,13 +2,5 @@ $(document).ready(function(){
 
     "use strict";
 
-function dateFunction() {
-    var timestamp = new Date();
-    var utcDate = timestamp.toLocaleDateString();
-    $("<p> ${utcDate} </p>").appendTo("body");
-};
-
-dateFunction();
-
-    $("<p id='append'>&copy; 2022 Bildad. <br> All rights reserved</p>").appendTo("body");
+    $("<p>&copy; 2022 Bildad. <br> All rights reserved</p>").appendTo("body");
 });
